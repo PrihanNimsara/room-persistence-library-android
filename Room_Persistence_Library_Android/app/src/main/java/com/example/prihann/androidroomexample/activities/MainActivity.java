f87729525219e5a9cc9f712d541d37b96233fa53
Prihan Nimsara <Iamprihan@gmail.com>
2018-12-23 22:29:39 +0530

    example

--- Room_Persistence_Library_Android/app/src/main/java/com/example/prihann/androidroomexample/activities/MainActivity.java
+++ Room_Persistence_Library_Android/app/src/main/java/com/example/prihann/androidroomexample/activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.prihann.androidroomexample.R;
+import com.example.prihann.androidroomexample.model.Student;
+import com.example.prihann.androidroomexample.repo.StudentRepo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Context  context = getApplicationContext();
+//        Context  context = getApplicationContext();
+
+//        StudentRepo studentRepo = new StudentRepo(context);
+//        Student student = new Student();
+//        student.setStudentId("me123");
+//        student.setStudentName("prian");
+//        student.setStudentAge(22);
+//        student.setStudentCountry("LK");
+//        studentRepo.create(student);
     }
 
 
